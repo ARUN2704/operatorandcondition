1c60d011c1d27736b09f84ca4c07c02315c319ef
ARUN2704 <arunarun45674567@gmail.com>
2023-02-28 12:42:08 +0530

    addoperatorandcondition

---
+++ main.js
@@ -0,0 +1,94 @@
+   //expression  operator//
+
+var a=2, b=3, c=5;
+console.log(a+b*c);
+
+var d=55, e=65, f=75;
+console.log(d/e*f);
+
+
+var g=15, h=25, i=35, j=45, k=55;
+console.log(g/h*i+j-k);
+
+var l=0.5 ,m=0.7, n=0.8 ,o=9;
+console.log(l%m/n+o);
+
+var p=4 , q=14 , r=24, s=34;
+console.log(p%q%r%s);
+
+var t=100 , u=200 , v=300 , w=400, x=500 , y=600 , z=700;
+console.log(t*u-v+w%x/y*z);
+  
+  
+var ab=2 , cd=5;
+console.log(ab**cd);
+
+var ef=30 , gh=50;
+console.log(ef++  -gh);
+console.log(ef++  -gh);
+
+var ij=60 , kl=70;
+console.log(ij-- +kl);
+console.log(ij-- +kl);
+
+
+var mn=100 , op=200;
+console.log(++mn -op);
+
+
+var qr=45 , st=67;
+console.log(--qr +st);
+
+
+
+
+
+
+
+//comparison operator//
+var uv=7 , wx=6 , yz=5 , mm="4" , pm="murugan" , pmm="manimegala";
+
+
+
+console.log(uv>wx);
+
+console.log(yz<wx);
+
+console.log(yz<=wx);
+
+console.log(uv>=wx);
+
+console.log(yz==mm);
+
+console.log(mm===wx);
+
+console.log(yz!=mm);
+
+console.log(wx!==mm);
+
+console.log(wx==mm===uv!=wx);
+
+console.log(uv=wx);
+
+console.log(pm<pmm);
+
+console.log(pmm>pm);
+
+
+
+
+
+
+
+
+
+
+//popup box//
+
+alert('HI ARUN');
+var age=prompt("ENTER YOUR AGE");
+
+
+
+
+
